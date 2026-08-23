247832aeaa59899d3e1e349ea453cfcb6b669fc6
Yisak Abraham <yisakabraham10@gmail.com>
2026-08-23 18:35:03 +0300

    made some changes

--- src/routes.py
+++ src/routes.py
@@ -1,8 +1,9 @@
-from pydantic import BaseModel
-import datetime
+from fastapi import APIRouter, status
+from src.routes import Receipt
 
-class Receipt(BaseModel):
-    bank_type: str
-    date_of_transaction: datetime
-    transaction_sender: str
-    amount: float
+receiptrouter = APIRouter()
+
+@receiptrouter.post("/verify_receipt", status_code= status.HTTP_201_CREATED)
+async def receinved_receipt(receipt: Receipt):
+    
+    received_receipt = receipt.model_dump()
--- src/schemas.py
+++ src/schemas.py
@@ -1,9 +1,8 @@
-from fastapi import APIRouter, status
-from src.routes import Receipt
+from pydantic import BaseModel
+import datetime
 
-receiptrouter = APIRouter()
-
-@receiptrouter.post("/verify_receipt", status_code= status.HTTP_201_CREATED)
-async def receinved_receipt(receipt: Receipt):
-    
-    received_receipt = receipt.model_dump()
+class Receipt(BaseModel):
+    bank_type: str
+    date_of_transaction: datetime
+    transaction_sender: str
+    amount: float
