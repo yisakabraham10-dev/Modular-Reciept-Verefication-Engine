6a2c59e6730a43e33d1848fa5307c7b92907e9f2
Yisak Abraham <yisakabraham10@gmail.com>
2026-08-24 10:08:35 +0300

    created the config.py, installed pydantic-settings

---
+++ src/config.py
@@ -0,0 +1,6 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_URL: str
+
+    model_config = SettingsConfigDict(env_file= ".env", extra= "ignore")
