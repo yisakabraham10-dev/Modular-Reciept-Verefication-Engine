9cc702a71a1885f745289b622ad8054e0e75b4de
Yisak Abraham <yisakabraham10@gmail.com>
2026-08-27 18:44:22 +0300

    succesful connection with the database and a table has been create

--- src/db/main.py
+++ src/db/main.py
@@ -11,6 +11,4 @@ engine = create_async_engine(
 
 async def connect_db():
     async with engine.begin() as conn:
-
-        await conn.run_sync(SQLModel.metadata.create_all 
-                            )
+        await conn.run_sync(SQLModel.metadata.create_all)
